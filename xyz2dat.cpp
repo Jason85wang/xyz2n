373e7a73c3642c5b8fae1e0ec189789246c3e324
Jason85wang <wangwangming535@163.com>
2020-04-06 20:04:29 +0800

    20200406 wang 20:04

--- xyz2dat.cpp
+++ xyz2dat.cpp
@@ -5,6 +5,7 @@
 #include <QDialog>
 #include <QMessageBox>
 #include <QFileDialog>
+#include <QFile>
 
 //命名规范
 //类名　首字母大写,单词和单词之间首字母大写
@@ -25,21 +26,21 @@ xyz2dat::xyz2dat(QWidget *parent)
     //设置窗口标题
     setWindowTitle("xyz2dat 1.0 wangwangming535@163.com modify:2020.4.1 ");
     //设置窗口固定大小
-    //    setFixedSize(800,600);
+//    setFixedSize(600,400);
     //增加xyz2dat按钮功能
     /**
     第一个自定义按钮功能(测试用)
     关闭程序
     **/
-    QPushButton *btn = new QPushButton;
-    btn->setParent(this);
-    btn->move(300,300);
-    btn->resize(100,50);
-    btn->setText("Exit");
-    //xyz2dat按钮功能定义
-    connect(btn,&QPushButton::clicked,this,[=](){
-        this->close();
-    });
+//    QPushButton *btn = new QPushButton;
+//    btn->setParent(this);
+//    btn->move(300,300);
+//    btn->resize(100,50);
+//    btn->setText("Exit");
+//    //xyz2dat按钮功能定义
+//    connect(btn,&QPushButton::clicked,this,[=](){
+//        this->close();
+//    });
 
     /**
     第二个按钮功能
@@ -47,7 +48,7 @@ xyz2dat::xyz2dat(QWidget *parent)
     **/
     QPushButton *btn2 = new QPushButton;
     btn2->setParent(this);
-    btn2->move(400,300);
+    btn2->move(10,300);
     btn2->resize(20,20);
     btn2->setText("about");
     //xyz2dat按钮功能定义
@@ -79,16 +80,41 @@ xyz2dat::~xyz2dat()
     qDebug() << "主程序退出! send to github";
 }
 
-//对取得的文件进行相应的数据处理
+/**对取得的文件进行相应的数据处理
+ *
+ *
+ * */
 void xyz2dat::fileReadxyz()
 {
-    //文件对话框
+    QByteArray array1;
+    double line_number = 0;  //读取的行数
+    QString linedata;
+    //读取文件并放入控件中
+    QFile filein(fname);
+    filein.open(QIODevice::ReadOnly);
+    for (int var = 0; var < 100; ++var) {
+        array1 = filein.readLine();
+        ui->textEdit_org->insertPlainText(array1);
+        //识别与处理数字
+        linedata=array1;
 
+        QStringList str_list = linedata.split(QRegExp("\\W+"));
+        for (int i=0; i<str_list.length(); ++i) {
+             ui->textBro_out->append(str_list[i]);
+            ui->textBro_out->append(str_list[i]);
+
+        }
+        ++line_number;
+//        ui->textEdit_org->insert(array1);
+    }
+
+    filein.close();
     qDebug() <<" 打开文件名为:"<<fname;
 }
 
-//处理的第一步,选择文件,并取得文件路径与文件名
-//参数一,文件类型过滤,默认为所有文件
+/**　处理的第一步,选择文件,并取得文件路径与文件名
+*参数一,文件类型过滤,默认为所有文件
+**/
 void xyz2dat::fileOpenx(int type=0)
 {
     QString filefilt;
@@ -97,12 +123,14 @@ void xyz2dat::fileOpenx(int type=0)
         case 1:
             //xyz与dat格式互转
             filefilt = tr("xyz data Files (*.xyz *.dat *.txt)");
+        break;
     default:
         filefilt = tr("all Files (*.*)");
     }
     fname = QFileDialog::getOpenFileName(this,"打开文件","\\",filefilt );
     //将文件名显示在状态栏中
     ui->statusbar->showMessage(fname);
+    //输出调试信息
     qDebug() <<" 打开文件名为:"<<fname;
 }
 void xyz2dat::showmodifyinfo(QString info1)
@@ -143,4 +171,6 @@ void xyz2dat::showmodifyinfo(QString info1)
 void xyz2dat::on_Btn_xyz2dat_clicked()
 {
     fileOpenx(1);
+    //对数据进行处理
+    fileReadxyz();
 }
