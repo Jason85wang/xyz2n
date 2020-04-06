0e9bbdfe838c46f67f714c0b180ecb7bfd519bbc
Jason85wang <wangwangming535@163.com>
2020-04-06 16:13:07 +0800

    20200406-wang

--- xyz2dat.cpp
+++ xyz2dat.cpp
@@ -3,6 +3,8 @@
 #include <qpushbutton.h>
 #include <qdebug.h>
 #include <QDialog>
+#include <QMessageBox>
+#include <QFileDialog>
 
 //命名规范
 //类名　首字母大写,单词和单词之间首字母大写
@@ -25,6 +27,10 @@ xyz2dat::xyz2dat(QWidget *parent)
     //设置窗口固定大小
     //    setFixedSize(800,600);
     //增加xyz2dat按钮功能
+    /**
+    第一个自定义按钮功能(测试用)
+    关闭程序
+    **/
     QPushButton *btn = new QPushButton;
     btn->setParent(this);
     btn->move(100,10);
@@ -35,25 +41,39 @@ xyz2dat::xyz2dat(QWidget *parent)
         this->close();
     });
 
-    //第二个按钮功能
+    /**
+    第二个按钮功能
+    程序版本功能更新说明
+    **/
     QPushButton *btn2 = new QPushButton;
     btn2->setParent(this);
-    btn2->move(300,300);
+    btn2->move(400,300);
     btn2->resize(20,20);
     btn2->setText("about");
     //xyz2dat按钮功能定义
     connect(btn2,&QPushButton::clicked,this,[=](){
-        //使用自定函数处理
-        fileReadxyz();
+        //说明文件编辑状态
+        QString proinfo;
+        proinfo = "wangwangming535@163.com tel:13476457809\n";
+        proinfo.insert(1,"1-2020.4.1开始修改\n");
+        proinfo.insert(1,"2- 2020.4.4将程序同步至github\n");
+        proinfo.insert(1,"3- 2020.4.6增加对话框说明\n");
+        proinfo.insert(1,"2020.4.6增加对话框说明\n");
+        proinfo.insert(1,"2020.4.6增加对话框说明\n");
+        proinfo.insert(1,"2020.4.6增加对话框说明\n");
+        proinfo.insert(1,"2020.4.6增加对话框说明\n");
+
+        showmodifyinfo(proinfo);
     });
    //XYZ2DAT功能
     connect(ui->Act_axyz2dat,&QAction::triggered,this,[=](){
         //模态对话框
-        QDialog dlg(this);
-        dlg.resize(300,100);
-        dlg.exec();
-        qDebug() << "模态对话框　mo tai dialog.";
-
+//        QDialog dlg(this);
+//        dlg.resize(300,100);
+//        dlg.exec();
+//        qDebug() << "模态对话框　mo tai dialog.";
+        //使用自定函数处理
+        fileReadxyz();
     });
 }
 
@@ -66,11 +86,37 @@ xyz2dat::~xyz2dat()
 
 void xyz2dat::fileReadxyz()
 {
-    //非模态对话框
-    QDialog *dlg2 = new QDialog(this);
-    dlg2->resize(300,200);
-    dlg2->setAttribute(Qt::WA_DeleteOnClose); //55号属性,关闭后释放内存
-    dlg2->show();
+    //文件对话框
+    QString fname = QFileDialog::getOpenFileName(this,"打开文件","\\",tr("xyz data Files (*.xyz *.dat *.txt)") );
+    //将文件名显示在状态栏中
+    ui->statusbar->showMessage(fname);
+    qDebug() <<" 打开文件名为:"<<fname;
+}
+void xyz2dat::showmodifyinfo(QString info1)
+{
+    //消息对话框
+    //错误
+//     QMessageBox::critical(this,"错误窗口","说明");
+    //信息
+//    QMessageBox::information(this,"info title","info for this ");
+    //警告
+//    QMessageBox::warning(this,"警告","下面输出程序修改说明!");
+    //提问
+    if(QMessageBox::Ok == QMessageBox::question(this,"程序修改过程说明",info1,QMessageBox::Cancel|QMessageBox::Ok,QMessageBox::Cancel))
+    {
+        qDebug() << "选择的是ok";
+    }
+    else
+    {
+        qDebug() << "选择的是cancel";
+    }
 
-    qDebug() <<" this is file read function add to github";
+
+    //非模态对话框
+//    QDialog *dlg2 = new QDialog(this);
+//    dlg2->resize(300,200);
+//    dlg2->setAttribute(Qt::WA_DeleteOnClose); //55号属性,关闭后释放内存
+//    dlg2->show();
+
+//    qDebug() <<" this is file read function add to github";
 }
--- xyz2dat.h
+++ xyz2dat.h
@@ -2,6 +2,7 @@
 #define XYZ2DAT_H
 
 #include <QMainWindow>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class xyz2dat; }
@@ -20,7 +21,7 @@ signals:
 public slots:
     //早期需要写入此处,高级版本可以在public或全局下
     void fileReadxyz();
-
+    void showmodifyinfo(QString info1);  //显示程序版本文字说明
 private:
     Ui::xyz2dat *ui;
 };
